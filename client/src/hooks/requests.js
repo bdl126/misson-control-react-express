91bca88a90e88e6748c3077cea2fb1abcc0a0cb5
Bruno <bdelafontaine@92@gmail.com>
2022-06-25 20:52:36 -0400

    first commit

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,30 @@
+const API_URL = 'http://localhost:8000'
+
+async function httpGetPlanets() {
+  const res = await fetch(`${API_URL}/planets`)
+  const data = await res.json()
+  console.log(data);
+  return data
+}
+
+async function httpGetLaunches() {
+  // TODO: Once API is ready.
+  // Load launches, sort by flight number, and return as JSON.
+}
+
+async function httpSubmitLaunch(launch) {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
